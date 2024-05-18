0bad655ac8773e2fceb3eeb1a8b37bfe46000e8c
Henrique Bortolo <henrique.bortolo2007@gmail.com>
2024-05-18 12:48:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+n1 = float (input("digite o primeiro numero:"))
+n2 = float (input("digite o segundo numero:"))
+
+if n1 > n2:
+  print(f"{n1} e maior")
+elif n1 == n2:
+  print("os numeros sao iguais")
+else:
+  print(f"{n2} e o maior")
